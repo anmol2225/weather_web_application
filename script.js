b24904560a73d2f25d3281ea98b692ef85e94c73
anmol2225 <117629273+anmol2225@users.noreply.github.com>
2024-05-04 17:11:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,14 +14,10 @@ let weather = {
         const { icon, description } = data.weather[0];
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
-        const{ latitude } =data.latitude;
-        const{ longitude } =data.longitde;
         document.querySelector(".city").innerText = "Weather in " + name;
         document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
         document.querySelector(".description").innerText = description;
         document.querySelector(".temp").innerText = temp + "°C";
-        document.querySelector(".latitude").innerText = latitude + "°";
-        document.querySelector(".longitude").innerText = longitude + "°";
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
         document.querySelector(".wind").innerText = "Wind speed: " + speed + " km/h";
         document.querySelector(".weather").classList.remove("loading");
